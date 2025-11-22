1b09df8d479f4bcb8cf0833c2b4cd7bf60f05437
AntoineB0 <antoine.berteloot@etu.unilim.fr>
2025-11-22 12:50:18 +0100

    style: apply custom color palette and create main page layout

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import FieldManager from '@/components/FieldManager';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-linear-to-br from-deep-space-blue-50 to-dark-amethyst-100 py-8">
+      <FieldManager />
+    </div>
+  );
+}
